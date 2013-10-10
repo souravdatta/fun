95d7c6a4d0199bb825e86db0ec32b2ad69d214a0
Sourav Datta <soura.jagat@gmail.com>
2013-10-10 10:56:36 +0530

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -53,58 +53,58 @@ template <class T, int N>
 class Graph
 {
 public:
-	static const int SIZE = N;
+    static const int SIZE = N;
 
-	Graph()
-	{
-		for (int i = 0; i < N; i++) {
-			VI vec;
-			for (int j = 0; j < N; j++) {
-				vec.push_back(false);
-			}
-			G.push_back(vec);
-		}
-	}
+    Graph()
+    {
+        for (int i = 0; i < N; i++) {
+            VI vec;
+            for (int j = 0; j < N; j++) {
+                vec.push_back(false);
+            }
+            G.push_back(vec);
+        }
+    }
 
-	void addNode(const T& node)
-	{
-		this->actuals.push_back(node);
-	}
+    void addNode(const T& node)
+    {
+        this->actuals.push_back(node);
+    }
 
-	T getNodeAt(int i) const
-	{
-		if (i < 0 || i >= N)
-			throw "out of bounds";
-		return actuals[i];
-	}
+    T getNodeAt(int i) const
+    {
+        if (i < 0 || i >= N)
+            throw "out of bounds";
+        return actuals[i];
+    }
 
-	void connect(int i, int j)
-	{
-		if (i < 0 || i >= N)
-			throw "out of bounds 1";
-		if (j < 0 || j >= N)
-			throw "out of bounds 2";
-		G[i][j] = true;
-	}
+    void connect(int i, int j)
+    {
+        if (i < 0 || i >= N)
+            throw "out of bounds 1";
+        if (j < 0 || j >= N)
+            throw "out of bounds 2";
+        G[i][j] = true;
+    }
 
-	void printMatrix() const
-	{
-		VII::const_iterator it = G.begin();
-		for (int i = 0; it != G.end(); ++it, ++i) {
-			std::cout << i << "\t";
-			VI::const_iterator iit = it->begin();
-			for (; iit != it->end(); ++iit) {
-				std::cout << ((*iit)? 1 : 0) << " ";
-			}
-			std::cout << std::endl;
-		}
-	}
+    void printMatrix() const
+    {
+        VII::const_iterator it = G.begin();
+        for (int i = 0; it != G.end(); ++it, ++i) {
+            std::cout << i << "\t";
+            VI::const_iterator iit = it->begin();
+            for (; iit != it->end(); ++iit) {
+                std::cout << ((*iit)? 1 : 0) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 
 private:
-	typedef std::vector< std::vector<int> > VII;
-	typedef std::vector<int> VI;
-	std::vector<T> actuals;
-	VII G;
+    typedef std::vector< std::vector<int> > VII;
+    typedef std::vector<int> VI;
+    std::vector<T> actuals;
+    VII G;
 };
     
 
