b1ed647b4aa7a20346263e6dd9bc3db2b511e812
Sourav Datta <soura.jagat@gmail.com>
2015-07-20 13:29:04 +0530

    Create link_converter.py

---
+++ link_converter.py
@@ -0,0 +1,38 @@
+import re
+import sys
+import os
+
+LINK_FROM = r'(src|href)="http://learnyousomeerlang.com/' 
+LINK_TO = r'\1="'
+
+
+def convert_links(fname):
+    file = open(fname, 'rb')
+    content = file.read()
+    file.close()
+    basename = os.path.basename(fname)
+    dirname = os.path.dirname(fname)
+    nfname = os.path.join(dirname, basename + '_')
+    filew = open(nfname, 'wb')
+    content = re.sub(
+        LINK_FROM,
+        LINK_TO, 
+        content)
+    filew.write(content)
+    filew.close()
+    os.remove(fname)
+    os.rename(nfname, fname)
+
+def main():
+    start = sys.argv[1]
+    if not start:
+        start = '.'
+    for dp, dn, files in os.walk(start):
+        for f in files:
+            fname = os.path.join(dp, f)
+            print '[converting] ', fname
+            convert_links(fname)
+
+
+if __name__ == '__main__':
+    main()
